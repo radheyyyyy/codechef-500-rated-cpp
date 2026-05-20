104e01bd33f086a61cd4e37b32622d603e1eff28
Rajyavardhan Radhey <112493351+radheyyyyy@users.noreply.github.com>
2026-05-21 00:28:18 +0530

    Add basic 404 Not Found check in main function

---
+++ 404_Not_Found.cpp
@@ -0,0 +1,13 @@
+//404 Not Found
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x == 404) {
+        cout << "NOT FOUND";
+    }
+    else cout << "FOUND";
+}

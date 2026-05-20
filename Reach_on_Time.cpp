1b62433085e547a2a9a2d6b6923037bd28a90200
Rajyavardhan Radhey <112493351+radheyyyyy@users.noreply.github.com>
2026-05-21 00:32:08 +0530

    Implement time condition checker in Reach_on_Time.cpp
    This program reads multiple test cases and checks if the input value meets a specified time condition.

---
+++ Reach_on_Time.cpp
@@ -0,0 +1,20 @@
+//Reach on Time
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+        int T;
+        cin >> T;
+
+        while (T--) {
+            int X;
+            cin >> X;
+            if (X >= 30) {
+                cout << "Yes" << endl;
+            }
+            else {
+                cout << "No" << endl;
+            }
+        }
+}

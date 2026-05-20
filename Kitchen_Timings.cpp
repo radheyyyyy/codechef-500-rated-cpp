1214776eae83e95dd814ecade8faf8a378c4e926
Rajyavardhan Radhey <112493351+radheyyyyy@users.noreply.github.com>
2026-05-21 00:30:22 +0530

    Implement time difference calculation in Kitchen_Timings.cpp
    This program takes multiple test cases and calculates the time difference between two inputs.

---
+++ Kitchen_Timings.cpp
@@ -0,0 +1,15 @@
+//Kitchen Timings
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y, Z;
+        cin >> X >> Y;
+        cout << (Z = Y - X) << endl;
+    }
+}

5c3c0496e206820f051be89a51377d70348c5009
Rajyavardhan Radhey <112493351+radheyyyyy@users.noreply.github.com>
2026-05-21 00:29:37 +0530

    Add main function for Donation Drive calculations
    Implement main logic for Donation Drive program to calculate remaining donations.

---
+++ Donation_Drive.cpp
@@ -0,0 +1,18 @@
+//Donation Drive
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N, X;
+        cin >> N >> X;
+
+        cout << N - X << endl;
+    }
+
+
+}

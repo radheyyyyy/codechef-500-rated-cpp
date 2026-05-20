b6b192d92e46207ba32972d2f1d2b0f3ffdacd7d
Rajyavardhan Radhey <112493351+radheyyyyy@users.noreply.github.com>
2026-05-21 00:14:43 +0530

    Add basic Cricket World Cup Qualifier program

---
+++ Cricket_World Cup_Qualifier.cpp
@@ -0,0 +1,12 @@
+//Cricket World Cup Qualifier
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    if(x>=12){
+        cout<<"Yes";
+    }
+else cout<<"NO";
+}

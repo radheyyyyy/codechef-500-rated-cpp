da7b7e3f908434e8df86a7cf648c6138eea0de32
Rajyavardhan Radhey <112493351+radheyyyyy@users.noreply.github.com>
2026-05-21 00:31:15 +0530

    Add IPL Ticket Rush program logic
    Implement logic to calculate ticket excess based on input values.

---
+++ IPL_Ticket_Rush.cpp
@@ -0,0 +1,20 @@
+//IPL Ticket Rush
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, M;
+        cin >> N >> M;
+        if (N > M) {
+            cout << N - M << endl;
+        }
+        else {
+            cout << 0 << endl;
+        }
+    }
+}

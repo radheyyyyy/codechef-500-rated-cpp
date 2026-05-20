84aa44bb25d5b469c0dc0dba857e81313d0ed8ac
Rajyavardhan Radhey <112493351+radheyyyyy@users.noreply.github.com>
2026-05-21 00:19:31 +0530

    Add Saving_Taxes.cpp for tax calculation

---
+++ Saving_Taxes.cpp
@@ -0,0 +1,16 @@
+//Saving Taxes
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t > 0) {
+        int x, y;
+        cin >> x >> y;
+        cout << x - y << endl;
+        t--;
+    }
+
+}
